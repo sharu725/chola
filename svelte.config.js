328c28c41b15b0864a147eeb5128f4c8dfe0ee4a
sharath <sharath.dt@gmail.com>
2024-11-20 21:22:49 +0530

    try node version

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
